1e91a27d9e20699fdfc00d7b862614fa7d2c7980
wangmingli <wangmingli@autohome.com.cn>
2019-11-28 17:22:54 +0800

    router

---
+++ src/pages/m-end/router/index.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: mili
+ * @Date: 2019-11-26 16:09:30
+ * @LastEditTime: 2019-11-28 11:14:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /source_code/src/router/index.js
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import app from "../views/index.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "app",
+    component: app
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
